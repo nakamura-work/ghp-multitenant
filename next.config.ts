9e96e87ea7f54d3b683c12cfe03c7b1791552176
t.nakamura <nakamura@webstream.co.jp>
2025-01-17 14:30:41 +0900

    fix: specify document base

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,11 @@
 import type { NextConfig } from "next";
 
+const documentBase = process.env.DOCUMENT_BASE;
 const nextConfig: NextConfig = {
   output: 'export',
   distDir: 'dist',
+  basePath: documentBase ?? '',
+  assetPrefix: documentBase ?? '',
 };
 
 export default nextConfig;
